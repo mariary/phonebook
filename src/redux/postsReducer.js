2eaff84f13c345ddb2ca9d6cfcf779f14de8e588
mariarybakonenko <65123160+mariary@users.noreply.github.com>
2021-05-07 15:02:41 +0300

    update initial post

--- src/redux/postsReducer.js
+++ src/redux/postsReducer.js
@@ -1,7 +1,7 @@
 import {ADD_POST, DELETE_POST} from './types'
 
 const initialState = {
-    posts: [{name:'lol', id:'1', phone:'999'},{name:'haha', id:'2', phone:'999' }],
+    posts: [{name:'Alice', id:'1', phone:'8 800 222 35 35'},{name:'Mike', id:'2', phone:'666 66 66' }],
 }
 
 export const postsReducer = (state = initialState, action) => {
